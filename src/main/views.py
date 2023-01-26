281abf7cd260bccd6af456f7c3e6ddb86168f807
Gary <fishellgary@gmail.com>
2023-01-26 22:47:01 +0000

    Use python logging library

--- src/main/views.py
+++ src/main/views.py
@@ -3,8 +3,10 @@ from rest_framework.response import Response
 from nacl.signing import VerifyKey
 from nacl.exceptions import BadSignatureError
 from django.http import HttpResponseBadRequest, JsonResponse
+import logging
 
 class MainPokerBotApiView(APIView):
+    logger = logging.getLogger(__file__)
 
     def post(self, request):
         body = request.data
@@ -12,14 +14,14 @@ class MainPokerBotApiView(APIView):
         try:
             PUBLIC_KEY = '109f9036e7831400d46b3d44b7d3b7e3f837fe3c60e04427dfcf22cb204df8ef'
 
-            print(request.headers)
+            logger.error(request.headers)
             verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
             signature = request.headers["X-Signature-Ed25519"]
             timestamp = request.headers["X-Signature-Timestamp"]
             
-            print("Attempting signature verification")
+            logger.error("Attempting signature verification")
             verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
-            print("Verified signature")
+            logger.error("Verified signature")
         except (ValueError, BadSignatureError):
             return HttpResponseBadRequest(JsonResponse({'errorMessage': 'Invalid request signature'}))
         except KeyError:
@@ -28,5 +30,5 @@ class MainPokerBotApiView(APIView):
 
         # ACK ping coming from discord
         if body.get('type') == 1:
-            print("Got type with 1")
+            logger.error("Got type with 1")
             return Response({'type': 1}, status=200)
