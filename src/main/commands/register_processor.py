4dbbcc14fa4622a552cb562f2ba79ae9adbb1114
gfishell <fishellgary@gmail.com>
2023-03-02 15:43:49 -0800

    Added discriminator to response

--- src/main/commands/register_processor.py
+++ src/main/commands/register_processor.py
@@ -13,14 +13,14 @@ class RegisterCommandProcessor():
             player, created = Player.objects.get_or_create(user_id=user['id'])
             print(created)
             if not created:
-                return Helpers.message_response(f'@{user["username"]} you are already registered!'), True
+                return Helpers.message_response(f'@{user["username"]}#{user["discriminator"]} you are already registered!'), True
 
             else:
                 player.username = user['username']
                 player.discriminator = user['discriminator']
                 player.save()
                 logger.info(f'Registering user with ID={user["id"]} username={user["username"]}#{user["discriminator"]}')
-                return Helpers.message_response(f'@{user["username"]} you have been registered successfully!'), True
+                return Helpers.message_response(f'@{user["username"]}#{user["discriminator"]} you have been registered successfully!'), True
 
         except AttributeError as e:
             return "Invalid registration request. Missing fields: " + str(e), False
